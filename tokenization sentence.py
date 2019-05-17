498c3f22b109fe2b97035b134a62cbf398edafde
NabiAbdHadi <nabil.hadi69@gmail.com>
2019-05-17 21:06:42 +0800

    remove others array

--- tokenization sentence.py
+++ tokenization sentence.py
@@ -9,8 +9,6 @@ english = 0
 mixed = 0
 other = 0
 
-others = []  # initalize the empty array to insert the word/token that doesnt contain malay or english language
-
 with open("new file2", "r") as f:  # declare new file2 as f
     for index, sentence in enumerate(f, start=1):
         en_count = 0  # initialize the malay and english counter
@@ -27,7 +25,8 @@ with open("new file2", "r") as f:  # declare new file2 as f
             elif lang == 'indonesian':
                 bm_count += 1
             """problem 1 : the language_detection doesnt malay language however its detect the indonesian language. 
-            since the malay language and the indonesian doesnt have much different so i consider the indonesian is the malay"""
+            since the malay language and the indonesian doesnt have much different so i consider the indonesian
+            is the malay"""
 
         """start analysis the sentences is in which language"""
         if en_count > bm_count:  # if the english counter more than the malay so it'll consider english sentence
@@ -38,7 +37,6 @@ with open("new file2", "r") as f:  # declare new file2 as f
             malay += 1
         elif bm_count & en_count == 0:  # if the sentences doesnt contain any english or malay word so its consider other
             print(index, " :", sentence, " : other")  # because its might only contain punctuation only
-            others.append(word)
             other += 1
         else:
             if (
@@ -47,7 +45,6 @@ with open("new file2", "r") as f:  # declare new file2 as f
                 mixed += 1
             else:
                 print(index, " :", sentence, " : other")
-                others.append(word)
                 other += 1
 
 print()
@@ -57,9 +54,5 @@ print("malay : ", malay)
 print("other : ", other)
 print("mixed : ", mixed)
 
-others = list(dict.fromkeys(others))  # remove the repetitive elements in others array
-print()
-print("list of others ")
-print(others)
 """Problem 2 : some of the element in others array contain malay and english word however the language_detection.py 
 detect these word in other language for example "bijak" was detect as arabic"""
